cdbf07d2c3b2353339906c7e151b0c7951f3415a
graceseptyana <125243736+graceseptyana@users.noreply.github.com>
2023-02-13 16:06:03 +0700

    Add files via upload

---
+++ Praktek1.java
@@ -0,0 +1,17 @@
+
+package praktek1;
+
+/**
+ *
+ * @author User
+ */
+public class Praktek1 {
+    
+    public static void main(String[] args) {
+        System.out.println("Grace Septyana");
+        System.out.println("E41222805");
+        System.out.println("golongan B");
+        System.out.println("Lampung");
+    }
+    
+}
